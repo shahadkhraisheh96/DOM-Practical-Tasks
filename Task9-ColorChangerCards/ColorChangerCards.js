62358b59923f0a1ada3d0b02e40739de80b46fee
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-03-25 13:00:19 +0300

    task color change

---
+++ Task9-ColorChangerCards/ColorChangerCards.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("card-container");
+
+container.addEventListener("click", (event) => {
+  
+    const clickedCard = event.target.closest('.card');
+
+    if (!clickedCard) return;
+
+    const allCards = document.querySelectorAll('.card');
+    allCards.forEach(card => card.classList.remove('active'));
+    clickedCard.classList.add('active');
+});
